0dd8c56a674f36bbfa305f241a88e8225c1ac80e
Matthew OBrien <mobrien13@ycp.edu>
2024-11-18 14:33:46 -0500

    fix things in mr

--- src/repository/config/Models.js
+++ src/repository/config/Models.js
@@ -2,6 +2,8 @@ const User = require("../user/User");
 const Event = require("../event/Event");
 const EventParticipants = require('../event/EventParticipants');
 const StaffRoles = require('../user/StaffRoles');
+
+// add all new Sequelize models here
 const models = {
     User,
     Event,
