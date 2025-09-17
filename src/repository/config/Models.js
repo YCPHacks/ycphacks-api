042b69c558296d82030dbbdab1d331585679af4c
jrohrbaugh3 <jrohrbaugh3@ycp.edu>
2025-09-17 11:26:10 -0400

    #2: Add Prize table and associations

--- src/repository/config/Models.js
+++ src/repository/config/Models.js
@@ -3,11 +3,19 @@ const StaffRoles = require('../user/StaffRoles');
 const Event = require('../event/Event');
 const EventParticipant = require('../event/EventParticipant');
 const HackCategory = require('../event/HackCategory');
+const Prize = require('../event/Prize');
 const Sponsor = require('../sponsor/Sponsor');
 const SponsorTiers = require('../sponsor/sponsorTiers');
 const EventSponsor = require('../sponsor/EventSponsor');
 const Image = require('../image/Image');
 const Activities = require('../event/Activities');
+
+// Associations
+Prize.belongsTo(Event, { foreignKey: 'eventId', onDelete: 'CASCADE' });
+Prize.belongsTo(HackCategory, { foreignKey: 'categoryId', onDelete: 'SET NULL' });
+
+Event.hasMany(Prize, { foreignKey: 'eventId' });
+HackCategory.hasMany(Prize, { foreignKey: 'categoryId' });
 
 // add all new Sequelize models here
 const models = {
@@ -16,6 +24,7 @@ const models = {
     Event,
     EventParticipant,
     HackCategory,
+    Prize,
     Sponsor,
     SponsorTiers,
     EventSponsor,
