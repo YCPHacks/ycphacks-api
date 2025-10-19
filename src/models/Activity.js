593dc16e937cd3801ed9d4c7e9500f7b0fc1bae0
jrohrbaugh3 <jrohrbaugh3@ycp.edu>
2025-10-19 18:38:20 -0400

    #30: Add validations for activities

--- src/models/Activity.js
+++ src/models/Activity.js
@@ -12,6 +12,42 @@ class Activity {
         this.activityDescription = activityDescription;
         this.eventId = eventId;
     }
+
+    validate(isCreate = true) {
+        const errors = {};
+        const dateRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/; // Checks for 'YYYY-MM-DD hh:mm:ss' format
+
+        // 1. Validate id
+        if (!isCreate && (!this.id || this.id < 0)) {
+            errors.id = "ID is required for updating";
+        }
+
+        // 2. Validate name presence and length
+        if (!this.activityName || this.activityName.length > 255) {
+            errors.activityName = "Name is required and must be less than 256 characters";
+        }
+
+        // 3. Validate date presence, date is in valid format, and date is not in the past
+        if (!this.activityDate) {
+            errors.activityDate = "Date is required";
+        } else if (!dateRegex.test(this.activityDate)) {
+            errors.activityDate = "Invalid date format (expecting: YYYY-MM-DD hh:mm:ss AM/PM)";
+        } else if (Date.parse(this.activityDate) <= new Date()) {
+            errors.activityDate = "Date cannot be in the past"
+        }
+
+        // 4. Validate description length
+        if (this.activityDescription.length > 255) {
+            errors.activityDescription = 'Description must be less than 256 characters';
+        }
+
+        // 5. Validate event ID presence
+        if (!this.eventId || this.eventId < 0) {
+            errors.eventId = 'Event ID is required';
+        }
+
+        return errors;
+    }
 }
 
 module.exports = Activity
